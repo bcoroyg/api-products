014a61e8449e06a67fcd82bdfe22b24473e0cdbc
Emilio Coroy <becgonzalez25@gmail.com>
2022-08-18 15:19:06 -0600

    Seguridad con helmet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import helmet from 'helmet'
 import config from './config/index.js';
 import routerAPI from './routes/index.js';
 import { errorHandler, logErrors, wrapErrors } from './utils/middlewares/errorsHandlers.js';
 import notFoundHandler from './utils/middlewares/notFoundHandler.js';
+
 import Debug from "debug";
 const debug = Debug("app:server");
 const app = express();
 
+app.use(helmet());
 //Middlewares
 app.use(express.json());
 
